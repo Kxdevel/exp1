4fa61857465d3634a0494da31f7737af7d56b05c
Francisco Camargos <kxdevel@gmail.com>
2019-02-15 15:43:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require "./vendor/autoload.php";
+
+$umpreg = new \Exp1\Test123();
+$umpreg->alalao();
---
+++ src/Test123.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Exp1;
+
+class Test123 {
+
+    public function alalao() {
+        echo "Que calor";
+    }
+
+}
